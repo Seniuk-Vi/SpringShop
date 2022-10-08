8cf2b288465f3d6929202331bc7287e8396c6d93
seniuk <vitaliiseniuk04@gmail.com>
2022-10-08 12:56:23 +0300

    Initial commit

---
+++ src/main/java/com/epam/springshop/SpringShopApplication.java
@@ -0,0 +1,13 @@
+package com.epam.springshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/springshop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
