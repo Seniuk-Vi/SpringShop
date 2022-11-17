43955554c01a02e5fccf0ea1115800cfbdf88227
seniuk <vitaliiseniuk04@gmail.com>
2022-11-17 15:50:16 +0200

    HW_5 edited all services to JPA (not tested)

--- src/main/java/com/epam/springshop/service/UserService.java
+++ src/main/java/com/epam/springshop/service/UserService.java
@@ -8,7 +8,6 @@ public interface UserService {
 
      UserDto createUser(UserDto obj);
      UserDto getUser(Long obj);
-
      List<UserDto> getAllUsers();
      UserDto updateUser(long userId,UserDto obj);
      UserDto banUser(long userId);
